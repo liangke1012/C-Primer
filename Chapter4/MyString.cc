167db9224408807e271eb1fdc7e94d626c01708d
liangke <liangke15@huawei.com>
2024-04-01 17:30:58 +0800

    string

--- Chapter4/MyString.cc
+++ Chapter4/MyString.cc
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-    char cat[5] = {'a','b','c','d','\0'};
-    cout << cat << endl;
-    
+    const int Size = 15;
+    char name1[Size];
+    char name2[Size] = "C++owbay";
+
+    cout << "Howdy! i'm" << name2;
+    cout << "! What's your name?\n";
+    cin >> name1;
+    cout << "Well, " << name1 << ",your name has ";
+    cout << strlen(name1) << " letters and store\n";
+    cout << "in an array of " << sizeof(name1) << " bytes.\n";
+    cout << "your initial is " << name1[0] << " .\n";
+    name2[3] = '\0';
+    cout << name2 << endl;
     return 0;
 }
