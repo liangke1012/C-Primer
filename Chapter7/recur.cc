3f907af65f9b4f11c1dd3627c8373b8324eb6504
liangke <liangke15@huawei.com>
2024-04-03 15:45:06 +0800

    function

--- Chapter7/recur.cc
+++ Chapter7/recur.cc
@@ -16,3 +16,16 @@ void countdown(int n)
     }
     cout << n << " : Kaboom!\n";
 }
+
+/*
+Count down ... 4n at 0x61fe00
+Count down ... 3n at 0x61fdd0
+Count down ... 2n at 0x61fda0
+Count down ... 1n at 0x61fd70
+Count down ... 0n at 0x61fd40
+0 : Kaboom!
+1 : Kaboom!
+2 : Kaboom!
+3 : Kaboom!
+4 : Kaboom!
+*/
